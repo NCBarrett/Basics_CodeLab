e9f88f4b2a0c33a0fd2d2bc9360b68313c5d3fec
neillbarrett <barrettneill@gmail.com>
2023-12-08 03:18:42 -0800

    Started Compose Basics Step 13

--- app/src/main/java/com/example/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/example/basicscodelab/MainActivity.kt
@@ -16,6 +16,7 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -27,6 +28,7 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
@@ -94,6 +96,8 @@ private fun Greetings(
 private fun Greeting(name: String) {
     var expanded by remember { mutableStateOf(false) }
 
+    var contentDescription: String
+
     val extraPadding: Dp by animateDpAsState(
         if (expanded) 48.dp else 0.dp,
         animationSpec = spring(
@@ -115,10 +119,15 @@ private fun Greeting(name: String) {
                     fontWeight = FontWeight.ExtraBold)
                 )
             }
+            IconButton(onClick = { /*TODO*/ }) {
+                
+            }
             ElevatedButton(
                 onClick = { expanded = !expanded }
             ) {
-                Text(if (expanded) "Show Less" else "Show more")
+                contentDescription = if (expanded) stringResource(R.string.show_less)
+                    else stringResource(R.string.show_more)
+                Text(contentDescription)
             }
         }
     }
